d3d127fadc5b9e9a57bd3d7cfdbff1e1382edec2
anas124 <anasm9877@gmail.com>
2019-05-15 17:37:15 +0500

    Data entry

---
+++ Title/index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var path  = require('path');
+var mongoose = require('mongoose');
+var bodyParser= require('body-parser');
+var engine = require('consolidate');
+const data = require('./model/idea.js')
+
+var app= express();
+
+mongoose.connect('mongodb://localhost/dataa',{useNewUrlParser:true});
+const db = mongoose.connection;
+db.on(console.log('mongoose is working' ))
+db.once(err => {console.log('err')});
+
+
+app.set('views', __dirname + '/views');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+
+app.post('/goto',(req,res)=>{
+  var First_Name = req.body.firstName;
+  var Last_Name= req.body.lastName;
+  var Email_Address = req.body.emailAddress;
+  var Passwordd = req.body.password;
+
+  var details = new data({
+    FirstName: First_Name,
+    LastName: Last_Name,
+    EmailAddress: Email_Address,
+    Password: Passwordd
+  });
+
+  details.save((err)=>{
+    console.log("data is saved");
+    if (err){
+      console.log(err);
+    }
+  });
+
+  res.redirect("/")
+
+
+});
+
+app.post('/check',(req,res)=>{
+  var search=req.body.emailcheck;
+
+  var 
+})
+
+app.get('/sign_up',(req,res)=>{
+  res.sendFile(path.join(__dirname + '/views/index2.html'));
+})
+
+app.listen(8000,()=>{
+  console.log('server is running')
+});
