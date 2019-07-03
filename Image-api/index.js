f9cb4f94ab9bc303330015d88eff879062f366c1
anas124 <anasm9877@gmail.com>
2019-07-03 08:26:24 +0500

    heruko deploy

--- Image-api/index.js
+++ Image-api/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var path  = require('path');
+const port = process.env.PORT || 8000
+
 
 var app= express();
 
 app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname,'views')))
 
 
 app.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname + '/views/index.html'))
 })
 
-app.listen(8080,()=>{
-  console.log('server is running');
-})
+app.listen(process.env.PORT || 8080, () => {console.log('all is good')});
